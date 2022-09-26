8fe2b606baa1e01b23c66e2ca17a8457a32596df
ataide79 <106406979+ataide79@users.noreply.github.com>
2022-09-26 16:34:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "ranbir";
+var last_name = "kapoor";
+console.log(first_name.concat(last_name)); 
+var z=2;
+var y=4;
+var add= z + y ;
+console.log("soma="+add);
+
+var multi= z * y ;
+console.log("mult="+multi);
+var sub= z - y ;
+console.log("-="+sub);
+var div= z / y ;
+console.log("divs="+div);
